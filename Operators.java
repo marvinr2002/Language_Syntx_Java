24016bb33bf5b4636cd248cb1af6e758da4be0a6
Marvin Ruiz <116685708+marvinr2002@users.noreply.github.com>
2025-08-06 12:37:10 -0500

    C1 Operators.
    Nothing relevant so far...

---
+++ Operators.java
@@ -0,0 +1,21 @@
+public class Operators {
+    public static void main(String[] args) {
+        // Operators
+        // arithmetic
+        var a = 1;
+        var b = 5;
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+    // asignation
+        a = b;
+        System.out.println("Hello, " + a);
+        a = b * 2;
+        System.out.println(a);
+        
+
+    }
+
+}
