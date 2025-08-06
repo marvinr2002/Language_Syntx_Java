29b74732bd6d8dca42b047b4a278960ca9d4be78
Marvin Ruiz <116685708+marvinr2002@users.noreply.github.com>
2025-08-05 20:06:30 -0500

    c02
    Traditional data

---
+++ DataTypes.java
@@ -0,0 +1,25 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        // Primitive Data Types
+        // inter - naturales
+        int myInt = 23;
+        System.out.println(myInt);
+        //decimals
+        double myDouble = 1.72;
+        System.out.println(myDouble);
+        /*
+        float - long - byte
+         */
+        char myChar = 'A';
+        System.out.println(myChar);
+
+        boolean myBoolean = true;
+        System.out.println(myBoolean);
+
+        String myString = "Hola Java";
+        System.out.println(myString);
+        // Type of data during copilation time.
+        System.out.println(myBoolean);
+
+    }
+}
