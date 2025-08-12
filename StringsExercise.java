4509ed568aabfb5706f899226bb79b5a9283bdb6
Marvin Ruiz <116685708+marvinr2002@users.noreply.github.com>
2025-08-11 21:16:05 -0500

    Spanish String -
    StringBuffer or StringBuilder

---
+++ StringsExercise.java
@@ -0,0 +1,23 @@
+public class StringsExercise {
+    public static void main(String[] args) {
+        // 1. Concatena dos cadenas de texto.
+
+        // 2. Muestra la longitud de una cadena de texto.
+
+        // 3. Muestra el primer y último carácter de un string.
+
+        // 4. Convierte a mayúsculas y minúsculas un string.
+
+        // 5. Comprueba si una cadena de texto contiene una palabra concreta.
+
+        // 6. Formatea un string con un entero.
+
+        // 7. Elimina los espacios en blanco al principio y final de un string.
+
+        // 8. Sustituye todos los espacios en blanco de un string por un guión (-).
+
+        // 9. Comprueba si dos strings son iguales.
+
+        // 10. Comprueba si dos strings tienen la misma longitu
+    }
+}
