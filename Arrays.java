d3c2e05d455da372da225ebfa1d334324be3e50e
Marvin Ruiz <116685708+marvinr2002@users.noreply.github.com>
2025-09-21 23:37:39 -0400

    nomajorlearning
    -1

---
+++ Arrays.java
@@ -0,0 +1,29 @@
+public class Arrays {
+    public static void main(String[] args) {
+        // Creation
+        int [] numbers = new int[3];
+        System.out.println(numbers);
+        String [] names = {"Marvin","Ruiz","marvruir"};
+        System.out.println(names);
+        // acceso
+        System.out.println(names[0]);
+        System.out.println(numbers[0]);
+        System.out.println(numbers[1]);
+        int [] anos = new int[5] ;
+        //arrays list
+        System.out.println((new String[3])[0]);
+
+        // Modificacion
+
+        numbers[0] = 1;
+        numbers[1] = 54;
+        numbers[2] = 100;
+        System.out.println(numbers[1]);
+
+      
+
+
+
+
+    }
+}
