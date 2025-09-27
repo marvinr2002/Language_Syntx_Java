44fe4bf8e00336d9b090b824a3eb232ca63971d3
Marvin Ruiz <116685708+marvinr2002@users.noreply.github.com>
2025-09-27 15:16:54 -0400

    Add files via upload

--- foerach.java
+++ foerach.java
@@ -5,14 +5,32 @@ import java.util.Map;
 public class foerach {
     public static void main(String[] args){
         HashMap<String, Integer> Ages = new HashMap<>();
+        String[] names = {"Brais", "Moure", "mouredev"};
         Ages.put("Marvin", 25);
         Ages.put("Luis", 28);
         Ages.put("Marcelo", 30);
         System.out.println(Ages.size());
         System.out.println(Ages);
         for(Map.Entry<String, Integer> Age: Ages.entrySet()){
-            System.out.println(Age);
+            System.out.println(Age); 
         }
+ 
+        int index = 0;
+        while ( index < 5){
+            System.out.println(("Marvin gay"));
+            index++;
+        }
+
+        // do while 
+
+        // while
+        index = 0;
+        while(index < names.length){    
+            System.out.println(names[index]);
+            index++;
+        }
+
+        
     }
     
 }
