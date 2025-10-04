187b3e7889419eef15dd02a4bb7900ebcb0a2aca
Marvin Ruiz <116685708+marvinr2002@users.noreply.github.com>
2025-10-03 20:25:43 -0400

    Add files via upload

---
+++ FunctionLoopsMExercises.java
@@ -0,0 +1,37 @@
+public class FunctionLoopsMExercises{
+    public static void main(String[] args) {
+        String name = "Pablo";
+        welcomeToJava();
+        welcomeToJava(name);
+    }
+
+     // 1. Crea una función que imprima "¡Te doy la bienvenida al curso de Java desde cero!".
+
+     public static void welcomeToJava(){
+        System.out.println("Welcome to Java from zero to hero");
+     }
+
+        // 2. Escribe una función que reciba un nombre como parámetro y salude a esa persona.
+     public static  void welcomeToJava(String name){
+        System.out.println("Welcome to Java " + name);
+     }
+
+
+
+
+        // 3. Haz un método que reciba dos números enteros y devuelva su resta.
+
+        // 4. Crea un método que calcule el cuadrado de un número (n * n).
+
+        // 5. Escribe una función que reciba un número y diga si es par o impar.
+
+        // 6. Crea un método que reciba una edad y retorne true si es mayor de edad (y false en caso contrario).
+
+        // 7. Implementa una función que reciba una cadena y retorne su longitud.
+
+        //  8. Crea un método que reciba un array de enteros, calcula su media y lo retorna.
+
+        // 9. Escribe un método que reciba un número y retorna su factorial.
+
+        // 10. Crea una función que reciba un ArrayList<String> y lo recorra mostrando cada elemento.
+}
